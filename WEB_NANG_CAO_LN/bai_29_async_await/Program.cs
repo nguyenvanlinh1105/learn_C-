7a1e5b8722c5cc09a3926b0083903e116a79ceaf
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-07-27 23:58:12 +0700

    update proj bai_20_async_await

--- WEB_NANG_CAO_LN/bai_29_async_await/Program.cs
+++ WEB_NANG_CAO_LN/bai_29_async_await/Program.cs
@@ -9,7 +9,7 @@ namespace bai_29_async_await
 {
     /*
      * Lập trình bất đồng bộ : Làm việc với lớp Task
-     *          
+3     *          
      */
     class Program
     {
@@ -38,6 +38,60 @@ namespace bai_29_async_await
             Console.ResetColor();
             Console.WriteLine("End....");
         }
+         static async Task Task2(){
+            Task t2 = new Task(
+                () =>
+                {
+                    DoSomeThing(10, "T2", ConsoleColor.Green);
+                }
+                );
+            t2.Start();
+            await t2;
+            // t2.Wait();
+            Console.WriteLine("T2 đã hoàng thành");
+            
+        }// trả về task và sau khi trả về thì nó thực thi trong đó luôn
+         static Task Task3()
+        {
+            Task t3 = new Task(
+                (object ob) =>{
+                string tenTacVu = (string)ob;
+                DoSomeThing(10, tenTacVu, ConsoleColor.Magenta);
+            },"T3"
+                );
+            t3.Start();
+            t3.Wait();
+            Console.WriteLine("T3 đã hoàn thành");
+            return t3;
+        }
+        static async Task<string> Task4()
+        {
+            Task<string> t4 = new Task<string>(
+                () =>
+                {
+                    DoSomeThing(5, "T4", ConsoleColor.Green);
+                    return "Return form t4";
+                }
+                );
+            t4.Start();
+            await t4;
+            Console.WriteLine("T4 hoàn thành");
+            //return t4;
+            // khi khai baó phương thưc với từ khóa asyn ở phương thức trả về giá trị thì không cần return task và trả về giá trị đúng với kieru giá trị định nghĩa mà task sẽ trả về.
+            return "T4 ne moi nguoi";
+        }
+        static Task<string> Task5() {
+            Task<string> t5 = new Task<string>(
+                (object ob) =>
+                {
+                    string text = (string)ob;
+                    DoSomeThing(5, text, ConsoleColor.Magenta);
+                    return "T5 đã return nè";
+                }, "Linh Nguyen Pro"
+                );
+            t5.Start();
+            return t5;
+        }
         static void Main(string[] args)
         {
             // synchronous: đồng bộ -
@@ -48,30 +102,50 @@ namespace bai_29_async_await
 
             //async: bất đồng bộ
             // khởi tạo tác vụ 
-            Task t2 = new Task(
-                () =>
-                {
-                    DoSomeThing(10, "T2", ConsoleColor.Green);
-                }
-                );
-            Task t3 = new Task(
-                (object obj) =>
-                {
-                    string tenTacVu = (string)obj;
-                    DoSomeThing(10, tenTacVu, ConsoleColor.Magenta);
-                },"T3"
-                );
+            //Task t2 =  Task2();
+            //Task t3 =  Task3();
 
             // sử dụng task vụ 
-            t2.Start();// thread
-            t3.Start();
-            DoSomeThing(2, "T1", ConsoleColor.Blue);
-            t2.Wait();// chờ cho task2 thực hiện xong mới đến lượt t3 
-            t3.Wait();
+            // t2.Start();// thread
+            // t3.Start();
+            //DoSomeThing(2, "T1", ConsoleColor.Blue);
+            // t2.Wait();// chờ cho task2 thực hiện xong mới đến lượt t3 
+            // t3.Wait();
             //Task.WaitAll(t2, t3);// liệt kê các tác vụ thực hiện theo lần lượt và hoành thành mới in ra câu hell world
+            //await t2;
+            //await t3;
+            //Task t4 = new Task<string>(Func<string>);// task này sẽ nhận một function và trả về đúng giá trị định nghĩa của task mà không có đối số nào truyền  vào trong delegate
+            //Task<string> t4 = new Task<string>(
+            //    ()=>
+            //    {
+            //        DoSomeThing(10, "T4", ConsoleColor.Green);
+            //        return "Return from t4";
+            //    });
+            // đã tạo ra một phương thức tương tự cách ở trên nên ta có thể rút gọn lại như sau .
+            Task<string> t4 = Task4();
+            //Task t5 = new Task<string>(Function< object, string>,string); delegate sẽ làm tham số của task, trong delegate nhận tham số là object1 và object2 - delegate trả về giá trị === với giá trị của Task và nhận
+            Task<string> t5 = new Task<string>(
+                (object obj) =>
+                {
+                    string text = (string)obj;
+                    DoSomeThing(2, text, ConsoleColor.Magenta);
+                    return text;
+                },"Linh nguyễn t5"
+                );// nó truyền vào delegate qua object 
+            
 
+            //t4.Start();
+            t4.Wait();
+            t5.Start();
+            Task.WaitAll(t4, t5);
             Console.WriteLine("hello world");
+            var kq4 = t4.Result;
+            var kq5 = t5.Result;
+            Console.WriteLine(kq4);
+            Console.WriteLine(kq5);
             Console.ReadKey();
+
+            //TH2: async/await có kiểu trả về 
         }
     }
 }
